c877e5c51a6127e5e745c8182d0a316cfe0ffadf
araff-16 <alex_raffa16@hotmail.com>
2021-11-03 18:52:10 +0000

    Spearated input functions frome play.js

--- play.js
+++ play.js
@@ -1,39 +1,7 @@
 const {connect} = require('./client')
+const {setupInput} = require('./input')
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  
-  stdin.on("data", handleUserInput);
-  
-  
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit(); 
-  }
-  if (key === '\u001B\u005B\u0041'){
-    console.log('Move: up')
-  }
-  if (key === '\u001B\u005B\u0043'){
-    console.log('Move: right')
-  }
-  if (key === '\u001B\u005B\u0042'){
-    console.log('Move: down')
-  }
-  if (key === '\u001B\u005B\u0044'){
-    console.log('Move: left')
-  }
-};
 
 setupInput()
